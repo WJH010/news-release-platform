f8c209e7c9f7ab13e12b962c43cd7d20be4d662f
WJH <1072523825@qq.com>
2025-08-03 11:50:34 +0800

    注释优化

--- internal/message/dto/messageDTO.go
+++ internal/message/dto/messageDTO.go
@@ -2,19 +2,19 @@ package dto
 
 import "time"
 
-// ListArticleRequest 文章列表查询请求参数
+// MessageListRequest 消息列表查询请求参数
 type MessageListRequest struct {
 	Page        int    `form:"page" binding:"omitempty,min=1"`              // 页码，最小为1
 	PageSize    int    `form:"page_size" binding:"omitempty,min=1,max=100"` // 页大小，1-100
 	MessageType string `form:"message_type"`                                // 消息类型
 }
 
-// 消息内容查询请求参数
+// MessageContentRequest 消息内容查询请求参数
 type MessageContentRequest struct {
 	MessageID int `form:"id" binding:"required,numeric"` // 消息ID，必须为数字
 }
 
-// 获取未读消息数请求参数
+// UnreadMessageCountRequest 获取未读消息数请求参数
 type UnreadMessageCountRequest struct {
 	MessageType string `form:"message_type"` // 消息类型
 }
